709264d2db75b89e62022fb404a6bfd8c6fb35c1
Mizuki <zibing.xin@outlook.com>
2025-01-30 23:46:14 -0500

    initialize

---
+++ TowerDefense/Assets/Plugins/Editor/MySetup.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public static class MySetup
+{
+    [MenuItem("Setup/Create Folders")]
+    public static void CreateMyFolder()
+    {
+        Folder.CreateFolder("_Project", "Animations", "Art", "Audio", "Fonts",
+        "Materials", "Prefabs", "ScriptableObjects", "Scripts", "Settings");
+        AssetDatabase.Refresh();
+    }
+    static class Folder
+    {
+        public static void CreateFolder(string root, params string[] folders)
+        {
+            var fullPath = Path.Combine(Application.dataPath, root);
+            foreach (string folder in folders)
+            {
+                var folderPath = Path.Combine(fullPath, folder);
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+            }
+        }
+    }
+}
