21ac97d5d4ee92b1acb56205df17f747ced54e45
Mizuki <zibing.xin@outlook.com>
2025-03-05 18:38:55 -0500

    Update WaveSpawner.cs

--- TowerDefense/Assets/_Project/Scripts/WaveSpawner.cs
+++ TowerDefense/Assets/_Project/Scripts/WaveSpawner.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,13 +20,24 @@ namespace DoomsDayDefense
         public List<Wave> waves;
         public Transform spawnPoint;
 
-        private int currentWaveIndex = 0;
+        private int currentWaveIndex = -1;
         private bool isSpawning = false;
+        private bool wavesStarted = false;
 
-        private void Update()
+        public void StartWaves()
         {
-            if (!isSpawning && currentWaveIndex < waves.Count)
+            if (!isSpawning && currentWaveIndex < waves.Count - 1)
             {
+                wavesStarted = true;
+                StartNextWave();
+            }
+        }
+
+        private void StartNextWave()
+        {
+            if (!isSpawning && currentWaveIndex < waves.Count - 1)
+            {
+                currentWaveIndex++;
                 StartCoroutine(SpawnWave(waves[currentWaveIndex]));
             }
         }
@@ -36,6 +46,8 @@ namespace DoomsDayDefense
         {
             isSpawning = true;
 
+            Debug.Log("Spawning " + wave.waveName + " wave with " + wave.count + " enemies");
+
             for (int i = 0; i < wave.count; i++)
             {
                 SpawnEnemy(wave.enemyPrefab, wave.speedMultiplier);
@@ -43,7 +55,16 @@ namespace DoomsDayDefense
             }
 
             isSpawning = false;
-            currentWaveIndex++;
+
+            if (currentWaveIndex < waves.Count - 1)
+            {
+                yield return new WaitForSeconds(5f);
+                StartNextWave();
+            }
+            else
+            {
+                Debug.Log("All waves have been completed");
+            }
         }
 
         private void SpawnEnemy(GameObject enemyPrefab, float speedMultiplier)
